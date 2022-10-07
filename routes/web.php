e4bbf1fae9c19b8144052edd8f720518cb763be8
Luca Somaschini <somas99@gmail.com>
2022-10-07 21:59:39 +0200

    Added model, migration, executed migration

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,9 @@ Route::middleware('auth') // controlla se il visitatore ha l'autorizzazione per 
 
 // la seguente route va inserita per ultima in questo file.
 // è una rotta di fallback che mappa tutte le rotte non intercettate dalle istruzioni precedenti.
+// {any?} == qualsiasi URI\url (anche ad es: <localhost:8000/aeurygbjka>) che non sia rientrato tra le routes amministrative soprastanti (tramite il filtraggio),
+// viene reindirizzato verso la view <\guest\home.blade.php>, la quale contiene un'istanza di Vue,
+// quindi verrà visualizzato il codice contenuto in \resources\js\views\app.vue.
 Route::get("{any?}", function() {
     return view("guest.home");
 })->where("any", ".*");
