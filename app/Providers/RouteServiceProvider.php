b14f5d5c47f6e089bf64d6d929eb567ebf46bda7
Luca Somaschini <somas99@gmail.com>
2022-10-07 17:39:18 +0200

    Added: home contr., admin routes rules, comments

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/admin'; // gli utenti loggati vengono reindirizzati alla route /admin.
 
     /**
      * Define your route model bindings, pattern filters, etc.
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Auth; // aggiunto per non visualizzare errore in vscode a riga 22, nonostante Auth sia importato in automatico
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,4 +21,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// si raggruppano tutte le routes relative alla parte amministrativa del sito
+// devono, per essere considerate routes amministrative, soddisfare tutti i campi sottostanti (le seguenti funzioni fungono da filtro per l'accesso):
+// esempio: se non ho l'autorizzazione, si passa oltre andando a recuperare altre routes che non saranno amministrative.
+Route::middleware('auth') // controlla se si ha l'autorizzazione per proseguire, accedendo a queste routes (l'autorizzazione c'è dopo aver eseguito il login, viceversa non c'è).
+    ->namespace('Admin') // cerca i controller solo nella cartella 'Admin' dei controller: \app\http\controllers\ADMIN.
+    ->name('admin.') // cerca solo le routes i cui nomi iniziano con 'admin.'.
+    ->prefix('admin') // cerca solo le routes che hanno nella parte iniziale dell'url '/admin'
+    ->group(function () { // raggruppa tutte le route che soddisfano le condizioni precedenti
+        Route::get('/', 'HomeController@Index')->name('home');  //
+    });
